f821bd68608e2b1c70736b057df5e304cfef8c3d
Aditya Raj <adityarj729@gmail.com>
2021-07-13 12:40:15 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define all(a)      (a).begin(),(a).end()
+#define rep(i,a,n)   for( int i=a ; i<n ; i++)
+#define M 1000000007
+#define N 100005
+using namespace std;
+
+int main(){
+    int a=6 ,b=9;
+    cout<<a+b;
+}
